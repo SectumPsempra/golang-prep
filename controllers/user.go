dea5887f09da1ebe147e5f02dd20c49af1eab8de
shanurrahman-rapido <shanur.rahman@rapido.bike>
2021-06-06 02:33:07 +0530

    writing arbitrary data from request body using jsonparser library

--- controllers/user.go
+++ controllers/user.go
@@ -2,9 +2,13 @@ package controllers
 
 import (
 	"context"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 
+	"github.com/buger/jsonparser"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -28,15 +32,58 @@ func (u *User) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == http.MethodPost {
+		paths := [][]string{
+			{"uuid"},
+			{"tz"},
+			{"ua"},
+			{"st"},
+		}
+
+		result := parseJSON(r.Body, paths)
+
 		testCollection := u.database.Collection("BooksRead")
 
-		inserRes, err := testCollection.InsertOne(context.TODO(), Book{Name: "Some Potter", PublisherID: "IBN123", Cost: "1232", StartTime: "2013-10-01T01:11:18.965Z", EndTime: "2013-10-01T01:11:18.965Z"})
+		inserRes, err := testCollection.InsertOne(context.TODO(), bson.D{
+			{"ua", result.ua},
+			{"tz", result.tz},
+			{"uuid", result.uuid},
+		})
+
 		log.Println("InsertResponse : ", inserRes)
 		log.Println("Error : ", err)
 
 	}
 
 	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+type SmallPayload struct {
+	tz   string
+	st   string
+	ua   string
+	uuid string
+}
+
+var data SmallPayload
+
+func parseJSON(body io.ReadCloser, paths [][]string) SmallPayload {
+	b, _ := ioutil.ReadAll(body)
+	jsonparser.EachKey(b, func(idx int, value []byte, vt jsonparser.ValueType, err error) {
+		switch idx {
+		case 0:
+			data.uuid = string(value)
+		case 1:
+			v, _ := jsonparser.ParseString(value)
+			data.tz = string(v)
+		case 2:
+			data.ua = string(value)
+		case 3:
+			v, _ := jsonparser.ParseString(value)
+			data.st = string(v)
+		}
+	}, paths...)
+
+	return data
 }
 
 func NewUser(l *log.Logger, database *mongo.Database, ctx context.Context) *User {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/SectumPsempra/golang-prep
 go 1.16
 
 require (
+	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/nicholasjackson/env v0.6.0 // indirect
 	go.mongodb.org/mongo-driver v1.5.3 // indirect
 )
